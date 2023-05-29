de79397b97100f1ef4d45a34d85fcb0beab8dcb7
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-29 16:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  arr.sort((a,b)=>a-b);
+  //Using two pointers Technique
+if(arr.length<2)
+{
+	return 0;
+}
+  let 3sum = Number.MAX_VALUE;
+	for(let i=0;i<arr.length-2;i++)
+		{
+			let j=i+1;
+			let k=arr.length-1;
+
+			while(j<k)
+				{
+					let sum=arr[i]+arr[j]+arr[k];
+					if(Math.abs(target-3sum)>Math.abs(target-sum))
+					{
+						3sum=sum;
+					}
+
+					if(sum>target)
+					{
+						k--;
+					}
+					else
+					{
+						j++;
+					}
+				}
+		}
+	return 3sum;
 }
 
 module.exports = threeSum;
